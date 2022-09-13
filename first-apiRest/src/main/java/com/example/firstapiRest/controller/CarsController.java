ea256b4947afa7ad5f050d5cd5dfffb33e901ffc
martinmramos <xitej091@gmail.com>
2022-09-13 18:19:15 +0200

    Prueba de endpoints

--- first-apiRest/src/main/java/com/example/firstapiRest/controller/CarsController.java
+++ first-apiRest/src/main/java/com/example/firstapiRest/controller/CarsController.java
@@ -1,2 +1,29 @@
-package com.example.firstapiRest.controller;public class CarsController {
+package com.example.firstapiRest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarsController {
+
+    @GetMapping("/cars")
+    public void getCars(){
+        System.out.println("Aquí tiene el listado de coches.");
+    }
+
+    @GetMapping("/car/{id}")
+    public void getCarById(@PathVariable String id){
+        System.out.println("Mostrando los datos del coche con ID " + id);
+    }
+
+    @GetMapping("/car/{id}/{owner}")
+    public void getOwnerByCarId (@PathVariable String id, @PathVariable String owner){
+        System.out.println("El propietario del coche con ID " + id + " es: " + owner);
+    }
+
+    @GetMapping("/car/{id}/exposition/{num}")
+    public void getCarByIdAndExpositionByNumber(@PathVariable String id, @PathVariable String num){
+        System.out.println("Los datos del coche " + id + " en la exposición " + num + " son los siguientes.");
+    }
 }
--- first-apiRest/src/main/java/com/example/firstapiRest/controller/ExpositionController.java
+++ first-apiRest/src/main/java/com/example/firstapiRest/controller/ExpositionController.java
@@ -1,2 +1,20 @@
-package com.example.firstapiRest.controller;public class ExpositionController {
+package com.example.firstapiRest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpositionController {
+
+    @GetMapping("/expositions")
+    public void getExpositions() {
+        System.out.println("Aquí tiene el listado de exposiciones.");
+    }
+
+    @GetMapping("/exposition/{num}/cars")
+    public void getCarsByExposition(@PathVariable String num){
+        System.out.println("Los coches de la exposición " + num + " son los siguientes.");
+    }
+
 }
